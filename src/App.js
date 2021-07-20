4476a817c165e8a42147638818675531c87a7d2c
manuelcarrascozuleta <manuelcarrascozuleta@gmail.com>
2021-07-20 19:26:07 -0300

    cambie el titulo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Proyecto innect 
+          Proyecto innect primer clase
         </p>
         <a
           className="App-link"
